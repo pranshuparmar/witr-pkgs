32aad35a3dd0487440c75fd63bab3169086b6239
Pranshu Parmar <pranshu.parmar@gmail.com>
2026-02-06 18:09:12 +0530

    choco auto push

--- choco/update_all.ps1
+++ choco/update_all.ps1
@@ -1,5 +1,8 @@
 Import-Module au
 $ErrorActionPreference = 'Stop'
+
 $au_root = $PSScriptRoot
 
-ls $au_root -Recurse -Filter update.ps1 | Update-AUPackages -Options @{ Push = $true }
+ls $au_root -Recurse -Filter update.ps1 | Update-AUPackages
+
+choco push "$au_root\**\*.nupkg" --source https://push.chocolatey.org/
