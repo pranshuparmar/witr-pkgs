0de346339cb45905b5251353d98395999ba691e5
Pranshu Parmar <pranshu.parmar@gmail.com>
2026-02-06 13:04:42 +0530

    Naming fixes

--- choco/witr/update.ps1
+++ choco/witr/update.ps1
@@ -1,32 +1,35 @@
 import-module au
+$ErrorActionPreference = 'Stop'
 
 $releases = 'https://api.github.com/repos/pranshuparmar/witr/releases/latest'
 
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyinstall.ps1" = @{
-            "(?i)(^\s*`$url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*`$checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^\s*`$urlArm64\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*`$checksumArm64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*`$url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*`$checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*`$urlArm64\s*=\s*)('.*')"     = "`$1'$($Latest.URLARM64)'"
+            "(?i)(^\s*`$checksumArm64\s*=\s*)('.*')"= "`$1'$($Latest.ChecksumARM64)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $json = $download_page.Content | ConvertFrom-Json
+    $json = Invoke-WebRequest -Uri $releases | ConvertFrom-Json
 
     $url64 = $json.assets | Where-Object { $_.name -like "*windows-amd64.zip" } | Select-Object -ExpandProperty browser_download_url
     $urlArm64 = $json.assets | Where-Object { $_.name -like "*windows-arm64.zip" } | Select-Object -ExpandProperty browser_download_url
     $version = $json.tag_name.TrimStart('v')
 
+    if (-not $url64) { throw "amd64 asset not found" }
+    if (-not $urlArm64) { throw "arm64 asset not found" }
+
     @{
-        Version    = $version
-        URL64      = $url64
-        URL32      = $urlArm64
-        FileType   = 'zip'
+        Version  = $version
+        URL64    = $url64
+        URLARM64 = $urlArm64
+        FileType = 'zip'
     }
 }
 
-Update-Package -ChecksumFor 64,32
+Update-Package -ChecksumFor all
