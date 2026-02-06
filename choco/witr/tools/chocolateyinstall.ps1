a5856da519dd9bb5670af3ad4a34f0536c7ff5f5
Pranshu Parmar <pranshu.parmar@gmail.com>
2026-02-06 10:26:49 +0000

    AU auto update

--- choco/witr/tools/chocolateyinstall.ps1
+++ choco/witr/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/pranshuparmar/witr/releases/download/v0.2.5/witr-windows-amd64.zip'
-$checksum64 = '59DE5A9E4541E6C2215D4519BEED21CB26C0BC28A6E88FD86E9B7AA6DDC20083'
+$url64      = 'https://github.com/pranshuparmar/witr/releases/download/v0.2.6/witr-windows-amd64.zip'
+$checksum64 = 'e95c3790d8e00a586bb780aecc2a5171786c8215dbdb9e239f33f6b0df709424'
 $checksumType64 = 'sha256'
 
-$urlArm64      = ''
-$checksumArm64 = ''
+$urlArm64      = 'https://github.com/pranshuparmar/witr/releases/download/v0.2.6/witr-windows-arm64.zip'
+$checksumArm64 = '200f2725d73e4e64f1732ba5056d6e17969bdeb9d8374b771ec4cd2867bdf7b8'
 $checksumTypeArm64 = 'sha256'
 
 $url = $url64
